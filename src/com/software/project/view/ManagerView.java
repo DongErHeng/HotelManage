0b6fc491613b2130a21375cf35a96a4e14ae8b33
dongErHeng <1710121009@vip.henu.edu.cn>
2019-11-06 21:45:20 +0800

    添加管理员

---
+++ src/com/software/project/view/ManagerView.java
@@ -0,0 +1,207 @@
+package com.software.project.view;
+
+import com.software.project.dao.RoomDao;
+import com.software.project.model.Room;
+import com.software.project.util.DBUtil;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class ManagerView extends JFrame {
+
+    private JPanel contentPane;
+    private JTable roomTable;
+    private DBUtil dbUtil = new DBUtil();
+    private RoomDao roomDao = new RoomDao();
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ManagerView frame = new ManagerView();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public ManagerView() {
+        setTitle("\u67E5\u8BE2");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 669, 558);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+
+        JButton btn_search = new JButton("\u67E5\u8BE2");
+        btn_search.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                btn_searchActionPerformed(evt);
+            }
+
+        });
+        btn_search.setForeground(new Color(139, 0, 0));
+        btn_search.setFont(new Font("华文行楷", Font.PLAIN, 28));
+
+        JScrollPane scrollPane = new JScrollPane();
+
+        JButton btn_return = new JButton(" \u9000\u51fa");
+        btn_return.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent evt) {
+                btn_returnMouseClicked(evt);
+            }
+
+        });
+        btn_return.setFont(new Font("华文行楷", Font.PLAIN, 28));
+
+        GroupLayout gl_contentPane = new GroupLayout(contentPane);
+        gl_contentPane.setHorizontalGroup(
+                gl_contentPane.createParallelGroup(Alignment.TRAILING)
+                        .addGroup(gl_contentPane.createSequentialGroup()
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+                                        .addGroup(gl_contentPane.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 603, GroupLayout.PREFERRED_SIZE)))
+                                .addContainerGap(24, Short.MAX_VALUE))
+                        .addGroup(gl_contentPane.createSequentialGroup()
+                                .addContainerGap(459, Short.MAX_VALUE)
+                                .addComponent(btn_return, GroupLayout.PREFERRED_SIZE, 144, GroupLayout.PREFERRED_SIZE)
+                                .addGap(38))
+                        .addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+                                .addGap(243)
+                                .addComponent(btn_search, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(258, Short.MAX_VALUE))
+        );
+        gl_contentPane.setVerticalGroup(
+                gl_contentPane.createParallelGroup(Alignment.LEADING)
+                        .addGroup(gl_contentPane.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 311, GroupLayout.PREFERRED_SIZE)
+                                .addGap(21)
+                                .addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+                                        .addComponent(btn_return)
+                                        .addComponent(btn_search))
+                        )
+        );
+
+        roomTable = new JTable();
+        roomTable.setModel(new DefaultTableModel(
+                new Object[][] {
+                },
+                new String[] {
+                        "\u8BA2\u5355\u7F16\u53F7",
+                        "\u8EAB\u4EFD\u8BC1\u53F7\u7801",
+                        "\u623F\u95F4\u7F16\u53F7",
+                        "\u5165\u4F4F\u65F6\u95F4",
+                        "\u5165\u4F4F\u5929\u6570",
+                        "\u79BB\u5F00\u65F6\u95F4"
+                }
+        ));
+        roomTable.setBorder(new LineBorder(new Color(178, 34, 34)));
+        scrollPane.setViewportView(roomTable);
+        contentPane.setLayout(gl_contentPane);
+    }
+    /**
+     * 返回主页面
+     * @param evt
+     */
+    private void btn_returnMouseClicked(MouseEvent evt) {
+        // TODO Auto-generated method stub
+        this.setVisible(false);
+        new LoginView().setVisible(true);
+    }
+    /**
+     * 订单查询
+     * @param evt
+     */
+    private void btn_searchActionPerformed(ActionEvent evt) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * 算账
+     * @param evt
+     */
+    private void btn_accountActionPerformed(ActionEvent evt) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * 初始化表格数据
+     * @param room
+     */
+    private void fillTable(Room room) {
+        // TODO Auto-generated method stub
+        DefaultTableModel dtm=(DefaultTableModel) roomTable.getModel();
+        dtm.setRowCount(0); // 设置成0行
+        Connection con=null;
+        try{
+            con=dbUtil.getCon();
+            ResultSet rs=roomDao.list(con, room);
+            while(rs.next()){
+                Vector v=new Vector();
+                v.add(rs.getInt("room_id"));
+
+                if ("0".equals(rs.getString("type"))){
+                    v.add("标准房");
+                } else if ("1".equals(rs.getString("type"))){
+                    v.add("高级房");
+                } else if ("2".equals(rs.getString("type"))){
+                    v.add("商务房");
+                }
+                v.add(rs.getString("price"));
+
+                if (rs.getBoolean("state")){
+                    v.add("已入住");
+                } else {
+                    v.add("空闲");
+                }
+                dtm.addRow(v);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try {
+                dbUtil.closeCon(con);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
+
